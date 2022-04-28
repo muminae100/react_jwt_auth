b18527ea3430cd07fadbfbd26fd329253e99a19a
foreverkenyan <smuminaetx100@gmail.com>
2022-04-28 20:12:16 +0300

    Create custom logger middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const path = require('path');
+const logEvents = require('./middleware/logEvents');
 
 const app = express();
+
+app.use((req, res, next) =>{
+    console.log(`${req.method} ${req.path}`);
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    next();
+});
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
