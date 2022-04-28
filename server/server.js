37892a742020e2b939dbf1d58d026572f22d3715
foreverkenyan <smuminaetx100@gmail.com>
2022-04-28 18:16:12 +0300

    Initialize express

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+})
+
+
+PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
