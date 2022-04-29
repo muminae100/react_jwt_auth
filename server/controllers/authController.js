6e8485e2cd2b509a58a0a71750c084cb45e108fe
foreverkenyan <smuminaetx100@gmail.com>
2022-04-29 18:08:20 +0300

    Create authController for authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,14 +6,23 @@ const userDb = {
     }
 }
 
-const handleAuth = (req, res) =>{
+const handleAuth = async (req, res) =>{
     const { name, psw } = req.body;
-    if(!name || !psw ) return res.status(400).json({"message": "Enter the required details!"});
+    if(!name || !psw ) return res.status(400).json({"message": "Fill all the required details!"});
 
-    const user = userDb.users.find(user => user.name === name && user.psw === bcrypt.compare(user.psw, psw));
-    if(!user) return res.status(409).json({"message": "Invalid crendentials!"});
+    const user = userDb.users.find(user => user.name === name);
 
-    res.json({user})
+    if(!user) return res.status(401).json({"message": "Invalid crendentials!"});
+
+    const match = await bcrypt.compare(psw, user.psw);
+
+    if(match){
+        //create jwt later
+
+        res.json({"success": `Login success for ${user.name}`})
+    }else{
+        res.sendStatus(401);
+    }
 }
 
 module.exports = { handleAuth };
